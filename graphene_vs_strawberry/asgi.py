d04e3fae2654590b1d43fe09d04d08e959b4c36f
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2023-08-08 23:08:27 +0900

    do black

--- graphene_vs_strawberry/asgi.py
+++ graphene_vs_strawberry/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphene_vs_strawberry.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphene_vs_strawberry.settings")
 
 application = get_asgi_application()
