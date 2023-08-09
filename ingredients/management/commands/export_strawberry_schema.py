d77e53641a21148d6577ddb49326182f40cb9155
Miyashita Yosuke <yosuke.miyashita@gmail.com>
2023-08-09 18:48:44 +0900

    add comment

--- ingredients/management/commands/export_strawberry_schema.py
+++ ingredients/management/commands/export_strawberry_schema.py
@@ -1,10 +1,16 @@
 from django.core.management import BaseCommand
 from strawberry.printer import print_schema
 
-from ingredients.schema_strawberry import schema  
+from ingredients.schema_strawberry import schema
 
 
 class Command(BaseCommand):
+    """Export the strawberry schema to a file
+    Because strawberry doesn't have a command to export the schema to a file,
+    we need to create one ourselves.
+    See https://github.com/strawberry-graphql/strawberry/issues/1583#issuecomment-1132720229
+    """
+
     help = "Exports the strawberry graphql schema"
 
     def handle(self, *args, **options):
